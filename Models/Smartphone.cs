90182b9c9e77179928af99481b60dab9d8773e41
Terencio Fonseca <terenciofonseca@protonmail.com>
2026-03-11 13:18:52 -0300

    Resolução baseada no diagrama

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,20 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private int Memoria { get; set; }
+
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("Snake");
